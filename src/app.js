d44c9dde797ed492b2d67184eb6af6d6177c98cd
Daniel Kulangiev <danny.kulangiev@gmail.com>
2019-05-14 16:59:00 -0400

    Add react-router to handle the routing of the application

--- src/app.js
+++ src/app.js
@@ -1,28 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {BrowserRouter, Route} from 'react-router-dom'
+import AppRouter from './routers/AppRouter'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
-const ExpenseDashboardPage = () => (
-    <div>
-        this is my dashboard component
-    </div>
-)
 
-const AddExpensePage = () => (
-    <div>
-        this is my add expense component
-    </div>
-)
-
-const routes = (
-    <BrowserRouter>
-        <div>
-            <Route path="/" component={ExpenseDashboardPage} exact={true}/>
-            <Route path="/create" component={AddExpensePage}/>
-        </div>
-    </BrowserRouter>
-)
-
-ReactDOM.render(routes, document.getElementById('app'))
+ReactDOM.render(<AppRouter/>, document.getElementById('app'))
