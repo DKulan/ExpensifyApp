77ef325885018c73bd9267d24a5075e5c0913850
Daniel Kulangiev <danny.kulangiev@gmail.com>
2019-05-16 11:03:20 -0400

    Add unconnected Redux library with test cases in app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import AppRouter from './routers/AppRouter'
+import configureStore from './store/configureStore'
+import {addExpense} from './actions/expenses'
+import {setTextFilter} from './actions/filters'
+import getVisibleExpenses from './selectors/expenses'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
+
+const store = configureStore()
+
+store.dispatch(addExpense({
+    description: 'Water bill',
+    amount: 150
+}))
+
+store.dispatch(addExpense({
+    description: 'Gas bill',
+    amount: 50
+}))
+
+store.subscribe(() => {
+    const state = store.getState()
+    const filter = getVisibleExpenses(state.expenses, state.filters)
+
+    console.log(filter)
+})
+
+store.dispatch(setTextFilter('gas'))
 
 
 ReactDOM.render(<AppRouter/>, document.getElementById('app'))
--- webpack.config.js
+++ webpack.config.js
@@ -12,8 +12,14 @@ module.exports = {
             use: {
                 loader: 'babel-loader',
                 options: {
-                    presets: ['@babel/preset-env', '@babel/preset-react'],
-                    plugins: ['transform-class-properties']
+                    presets: [
+                        '@babel/preset-env',
+                        '@babel/preset-react'
+                    ],
+                    plugins: [
+                        'transform-class-properties',
+                        'transform-object-rest-spread'
+                    ]
                 }
             }
         }, {
