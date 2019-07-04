66cf76bcd19c1bb6b2e6cf1c7cbb21fef960ef19
Daniel Kulangiev <danny.kulangiev@gmail.com>
2019-07-04 13:49:36 -0400

    Seperate test and dev database in Firebase. Abstract Firebase config info.

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,13 +2,12 @@ import * as firebase from 'firebase'
 
 
 const config = {
-  apiKey: "AIzaSyDmk82sLlDADw2nFiqoskxB_49BBai3q-U",
-  authDomain: "expensify-5da4e.firebaseapp.com",
-  databaseURL: "https://expensify-5da4e.firebaseio.com",
-  projectId: "expensify-5da4e",
-  storageBucket: "expensify-5da4e.appspot.com",
-  messagingSenderId: "483927480762",
-  appId: "1:483927480762:web:636ea33ec91761c3"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 }
 
 firebase.initializeApp(config)
